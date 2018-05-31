fb784d517b646c67d4980558f537b97531906010
nfuster2017 <33457770+nfuster2017@users.noreply.github.com>
2018-05-31 11:46:27 -0400

    timer
    Signed-off-by: nfuster2017 <33457770+nfuster2017@users.noreply.github.com>

--- time.py
+++ time.py
@@ -1,17 +1,70 @@
 import datetime as dt
 import time as t
+#gets start and end time
+def test_time():
+    hrs=int(input('hrs:'))
+    mins=int(input('mins:'))
+    start_time=dt.datetime.now()
+    tdelta=dt.timedelta(hours=hrs,minutes=mins)
+    end_time=start_time + tdelta
+    return end_time
 
-now=dt.datetime.now()
-year='0000'
-month='05'
-days='00'
-hrs='03'
-mins='02'
-sec='00'
-millsec='000000'
 
-t_time_plug=dt.timedelta(days=7)
-print(t_time_plug, now)
-print(now + t_time_plug)
+class StopWatch(tdelta):
+    """ Implements a stop watch frame widget. """
+
+    def __init__(self, parent=None, **kw):
+        tdelta.__init__(self, parent, kw)
+        self._start = 0.0
+        self._elapsedtime = 0.0
+        self._running = 0
+        self.timestr = StringVar()
+        self.makeWidgets()
+
+    def makeWidgets(self):
+        """ Make the time label. """
+        l = Label(self, textvariable=self.timestr)
+        self._setTime(self._elapsedtime)
+        l.pack(fill=X, expand=NO, pady=2, padx=2)
+
+    def _update(self):
+        """ Update the label with elapsed time. """
+        self._elapsedtime = time.time() - self._start
+        self._setTime(self._elapsedtime)
+        self._timer = self.after(50, self._update)
+
+    def _setTime(self, elap):
+        """ Set the time string to Minutes:Seconds:Hundreths """
+        minutes = int(elap / 60)
+        seconds = int(elap - minutes * 60.0)
+        hseconds = int((elap - minutes * 60.0 - seconds) * 100)
+        self.timestr.set('%02d:%02d:%02d' % (minutes, seconds, hseconds))
+
+    def Start(self):
+        """ Start the stopwatch, ignore if running. """
+        if not self._running:
+            self._start = time.time() - self._elapsedtime
+            self._update()
+            self._running = 1
+
+    def Stop(self):
+        """ Stop the stopwatch, ignore if stopped. """
+        if self._running:
+            self.after_cancel(self._timer)
+            self._elapsedtime = time.time() - self._start
+            self._setTime(self._elapsedtime)
+            self._running = 0
+
+    def Reset(self):
+        """ Reset the stopwatch. """
+        self._start = time.time()
+        self._elapsedtime = 0.0
+        self._setTime(self._elapsedtime)
+
+
+
+
+
+
 
 
