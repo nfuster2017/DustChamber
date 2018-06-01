80066dc020040e5de2a3ba2373bf2965684d04d3
nfuster2017 <33457770+nfuster2017@users.noreply.github.com>
2018-06-01 10:32:26 -0400

    getting timer to count backwards, adding hours, etc. need it to stop when it reaches zero.
    Signed-off-by: nfuster2017 <33457770+nfuster2017@users.noreply.github.com>

--- DUSTGUI.py
+++ DUSTGUI.py
@@ -9,7 +9,20 @@ class App():
         self.label.pack()
         self.update_clock()
         self.set_timer()
+        self.remaining = 0
+        self.countdown(10)
         self.root.mainloop()
+
+    def countdown(self, remaining=None):
+        if remaining is not None:
+            self.remaining = remaining
+
+        if self.remaining <= 0:
+            self.label.configure(text="time's up!")
+        else:
+            self.label.configure(text="%d" % self.remaining)
+            self.remaining = self.remaining - 1
+            self.after(1000, self.countdown)
 
     def update_clock(self):
         now = time.strftime("%H:%M:%S")
